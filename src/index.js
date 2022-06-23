ac69dfed4c8dcc1a6a269da27cd28497116dc60b
Ghostdied <ghostdied3@gmail.com>
2022-06-24 02:52:44 +0300

    feat: added implementation method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const [units, dozens, hundreds] = number.toString().split('').reverse();
+    if (number == 0) {
+        return 'zero'
+    }
+
+    console.log(units, dozens, hundreds);
+
+    const unitsArray = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    const universal = { 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen' };
+
+    const dozensArray = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let result = '';
+
+
+    if (hundreds) {
+        result += `${unitsArray[hundreds]} hundred `
+    }
+
+    if (dozens) {
+
+        if ((dozens + units) in universal) {
+            result += universal[dozens + units] + ' '
+
+        } else {
+            result += dozensArray[dozens - 1] ? (dozensArray[dozens - 1] + ' ') : ''
+        }
+    }
+
+    if (!((dozens + units) in universal) && units != 0) {
+        result += `${unitsArray[units]}`
+
+    } else if (!hundreds && !dozens && number.lenght === 1) {
+        result += 'zero'
+    }
+
+
+
+
+
+
+
+
+    return result.trim()
+
 }
